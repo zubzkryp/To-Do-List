aeeb2d87bec2d24b7f539331b6f30a0f2a1c8d95
Krxypton <krxypton@gmail.com>
2026-08-13 22:54:59 +0100

    Added entry output and plguins for webpack.common.js

---
+++ config/webpack.common.js
@@ -0,0 +1,22 @@
+ import path from 'node:path';
+ import { fileURLToPath } from 'node:url';
+ import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+ const __filename = fileURLToPath(import.meta.url);
+ const __dirname = path.dirname(__filename);
+
+ export default {
+   entry: {
+     app: './src/index.js',
+   },
+   plugins: [
+     new HtmlWebpackPlugin({
+       title: 'Production',
+     }),
+   ],
+   output: {
+     filename: 'main.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
+ };

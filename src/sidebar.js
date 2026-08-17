67c654897022ba33eb67d33430f81de712cce2f8
Krxypton <krxypton@gmail.com>
2026-08-17 21:57:44 +0100

    - Added tasks subsections and its css

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,4 +1,5 @@
 import './sidebar.css'
+import { createElement, Menu, ChevronsRight, ListChecks, Logs } from 'lucide'
 
 export default function buildSideBar() {
     const body = document.querySelector('body')
@@ -11,8 +12,59 @@ export default function buildSideBar() {
     dashBoard.classList.add('dashboard')
     sideBar.appendChild(dashBoard)
 
-    const dashboardTitle = document.createElement('div')
+    const dashboardTitle = document.createElement('h1')
     dashboardTitle.classList.add("dashboard-title") 
-    dashboardTitle.textContent = 'Menu'
     dashBoard.appendChild(dashboardTitle)
+
+    const menu = document.createElement('div')
+    menu.classList.add("dashboard-menu")
+    dashboardTitle.appendChild(menu)
+    menu.textContent = 'Menu'
+
+    // Adding menu icon onto an element
+    const menuIcon = createElement(Menu)
+    dashboardTitle.appendChild(menuIcon)
+
+    //small task title
+    const dashboardTask = document.createElement('h3')
+    dashboardTask.textContent = "TASKS"
+    dashboardTask.classList.add('dashboard-task')
+    dashBoard.appendChild(dashboardTask)
+
+    // upcoming 
+    const dashboardUpcoming = document.createElement('div')
+    dashboardUpcoming.classList.add("dashboard-upcoming")
+    dashBoard.appendChild(dashboardUpcoming)
+    //buttons to view list
+    const upcomingIcon = createElement(ChevronsRight) // icon for upcoming text
+    dashboardUpcoming.appendChild(upcomingIcon)
+    const upcoming = document.createElement('button')
+    upcoming.classList.add("upcoming")
+    upcoming.textContent = "Upcoming"
+    dashboardUpcoming.appendChild(upcoming)
+
+    // today tasks
+    const dashboardToday = document.createElement('div')
+    dashboardToday.classList.add("dashboard-today")
+    dashBoard.appendChild(dashboardToday)
+    //buttons to view today
+    const todayIcon = createElement(ListChecks) // icon for today text
+    dashboardToday.appendChild(todayIcon)
+    const today = document.createElement('button')
+    today.classList.add("today")
+    today.textContent = "Today"
+    dashboardToday.appendChild(today)
+
+     // This week tasks
+    const dashboardWeek = document.createElement('div')
+    dashboardWeek.classList.add("dashboard-week")
+    dashBoard.appendChild(dashboardWeek)
+    //buttons to view today
+    const weekIcon = createElement(Logs) // icon for today text
+    dashboardWeek.appendChild(weekIcon)
+    const week = document.createElement('button')
+    week.classList.add("week")
+    week.textContent = "Week"
+    dashboardWeek.appendChild(week)
+
 }
